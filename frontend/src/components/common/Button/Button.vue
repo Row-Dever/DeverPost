0458cb213864542dd560a33e422f9020ecccd449
Row-Dever <138644103+Row-Dever@users.noreply.github.com>
2023-07-13 16:51:59 +0900

    feat: 공통 Button 컴포넌트 생성

---
+++ frontend/src/components/common/Button/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="bg-mainOrange text-lightWhite rounded-lg px-6 py-2 text-sm flex gap-1 font-semibold hover:brightness-110 items-center justify-center"
+    :class="[
+      props.size === 'lg' ? 'w-80 py-3 px-8' : '',
+      props.disabled ? 'cursor-not-allowed' : ''
+    ]"
+    :disabled="disabled"
+    @click="props.onClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({ onClick: Function, size: String, disabled: Boolean })
+</script>
