9e80461e91720fe6a8355b7b4439dace33434395
Row-Dever <138644103+Row-Dever@users.noreply.github.com>
2023-07-13 16:53:04 +0900

    feat: Layout, Home page 생성 및 라우트 초기설정

---
+++ frontend/src/components/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="w-full h-16 fixed top-0 left-0 px-4 border-b border-gray-200">
+    <h1 class="ir">네비게이션</h1>
+    <Header></Header>
+  </header>
+  <main class="pt-16"><RouterView /></main>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import Header from './Header/Header.vue'
+</script>
---
+++ frontend/src/page/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="text-xl">
+    <Container>Home</Container>
+  </section>
+</template>
+
+<script setup>
+import Container from '../components/common/Container/Container.vue'
+</script>
