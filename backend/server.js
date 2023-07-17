3fcdec5ffb9744bcc679b3a7dc37c4cd556c6596
Row-Dever <138644103+Row-Dever@users.noreply.github.com>
2023-07-17 17:44:51 +0900

    init: 서버 초기세팅

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import postRouter from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
+
+const app = express();
+const port = 8800;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/post", postRouter);
+
+app.use("/user", userRouter);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`서버 localhost: ${port} 접속중..`);
+});
