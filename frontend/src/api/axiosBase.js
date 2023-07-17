87dc402b09261c6da255a51a544f8bddbc146b9b
Row-Dever <138644103+Row-Dever@users.noreply.github.com>
2023-07-17 17:38:52 +0900

    init: 기본 api설정 및 posts api 생성

---
+++ frontend/src/api/axiosBase.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const url = 'http://localhost:8800/'
+
+export const instance = axios.create({
+  baseURL: url,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+// 이미지 인스턴스
+export const imgInstance = axios.create({
+  baseURL: url,
+  headers: { 'Content-Type': 'multipart/form-data' }
+})
+
+// 로그인된 유저 인스턴스
+export const accessInstance = axios.create({
+  baseURL: url,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  withCredentials: true
+})
