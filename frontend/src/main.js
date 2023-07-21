db1194ede78b3c642aae74c4f8cbbc6ce7fbfb2e
Row-Dever <138644103+Row-Dever@users.noreply.github.com>
2023-07-21 18:25:00 +0900

    modify: pinia persist 기능 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,18 +2,21 @@ import './assets/main.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(fas)
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
 
 import App from './App.vue'
 import router from './router'
 
+library.add(fas)
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPersist)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -8,29 +8,20 @@ export const useUserStore = defineStore('user', {
     refreshToken: '' // 리프레시 토큰
   }),
   actions: {
-    // 로그인 성공 시 호출되는 액션
     loginSuccess(user, accessToken, refreshToken) {
+      console.log('login')
       this.loggedInUser = user
       this.accessToken = accessToken
       this.refreshToken = refreshToken
-
-      //로컬 스토리지에 토큰 저장 (페이지 리로드 시 유지하기 위해)
-      localStorage.setItem('accessToken', accessToken)
-      localStorage.setItem('refreshToken', refreshToken)
     },
-    // 로그아웃 시 호출되는 액션
     logout() {
       this.loggedInUser = null
       this.accessToken = ''
       this.refreshToken = ''
-
-      //로컬 스토리지에서 토큰 삭제
-      localStorage.removeItem('accessToken')
-      localStorage.removeItem('refreshToken')
-    },
-    persist: {
-      enabled: true,
-      strategies: [{ storage: localStorage }]
     }
+  },
+  persist: {
+    enabled: true,
+    strategies: [{ storage: localStorage }]
   }
 })
