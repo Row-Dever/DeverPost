c5a1fa4dac2a14ff9dd4e69dc44e7ca9dadcdfa2
Row-Dever <138644103+Row-Dever@users.noreply.github.com>
2023-07-21 11:06:40 +0900

    init: fontawesome 모듈 다운로드 및 main.js 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import './assets/main.css'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas)
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -7,6 +10,8 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
